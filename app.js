c702923e3211af6714bf6f16a361f4eb63610a4a
정훈박 <fireking5997@gmail.com>
2021-07-09 20:17:25 +0900

    app.js 파일 수정

--- app.js
+++ app.js
@@ -1,56 +1,64 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 3000
+const port = 3000;
 
+//session setting
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
+/////
 
-const connect = require('./schemas')
+const connect = require("./schemas");
 connect();
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 //javascript folder setting
 app.use("/static", express.static(__dirname + "/js"));
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.set('views', __dirname + "/views");
-app.set('view engine', 'ejs');
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
 
-const router = require('./routers/router');
-app.use('/', [router]);
+//인증 미들웨어
+// app.use(authMiddleWare);
+
+const router = require("./routers/router");
+app.use("/", [router]);
 
 // Making API
-const userRouter = require('./routers/user');
-app.use('/api/user', [userRouter]);
-const roomRouter = require('./routers/room');
-app.use('/api/room', [roomRouter]);
-const reviewRouter = require('./routers/review');
-app.use('/api/review', [reviewRouter]);
+const userRouter = require("./routers/user");
+app.use("/api/user", [userRouter]);
+const roomRouter = require("./routers/room");
+app.use("/api/room", [roomRouter]);
+const reviewRouter = require("./routers/review");
+const { authMiddleWare } = require("./middleWare");
+app.use("/api/room", [reviewRouter]);
+
+//예약기능 인기방 예약방 사진 가져오기
 const bookRouter = require("./routers/book");
 app.use("/api/book", [bookRouter]);
 
 //Session Setting
 app.use(
-    session({
-      secret: "secret",
-      resave: false,
-      saveUninitialized: false,
-      store: MongoStore.create({
-        mongoUrl: "mongodb://localhost/14hotel",
-      }),
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: "mongodb://localhost/14hotel",
+    }),
+    cookie: {
+      secure: true,
       cookie: {
-        secure: true,
-        cookie: {
-          //로그인은 하루동안 로그인 유지
-          maxAge: 1000 * 60 * 24,
-        },
+        //로그인은 하루동안 로그인 유지
+        maxAge: 1000 * 60 * 24,
       },
-    })
-  );
+    },
+  })
+);
 
 app.listen(port, () => {
-    console.log(`listening at http://localhost:${port}`);
-})
+  console.log(`listening at http://localhost:${port}`);
+});
