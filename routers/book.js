35912b5f0bc7c59312d6b98fb107988afa42d98d
정훈박 <fireking5997@gmail.com>
2021-07-09 20:17:57 +0900

    book 라우터 수정

--- routers/book.js
+++ routers/book.js
@@ -1,16 +1,17 @@
-// 정훈
-
 const express = require("express");
 const router = express.Router();
 const Book = require("../schemas/book");
 const Room = require("../schemas/review");
-
-//예약할때 고를 방 정보 넘겨주기 이거 필요없나?;;;;
-
 router.post("/", async (req, res) => {
-  const { date, adult, kis } = req.body;
+  const { date, adult, kid } = req.body;
   try {
-    const book = await Book.create({ date, adult, kis });
+    const book = new Book({
+      date,
+      adult,
+      kid,
+    });
+
+    await book.save();
     return res.json({ message: true, bookId: book._id });
   } catch (e) {
     console.log(e);
@@ -36,7 +37,7 @@ router.patch("/edit/:bookId", async (req, res) => {
   }
 });
 
-router.delete("/delete/:bookId", (req, res) => {
+router.delete("/delete/:bookId", async (req, res) => {
   const { bookId: _id } = req.params;
   try {
     const isExist = await Book.exists({ _id });
