5e58b9ef1950a032e791d0374ab9c13295c0c5a2
정훈박 <fireking5997@gmail.com>
2021-07-12 16:44:25 +0900

    [middleWare.js]인증미들웨어 추가

--- middleWare.js
+++ middleWare.js
@@ -1,21 +1,38 @@
 // exports.authMiddleWare = (req, res, next) => {
 //     res.locals.isLoggedIn = req.session.isLoggedIn;
 //     res.locals.usernickname = req.session.usernickname;
-  
+
 //     return next();
 //   };
-  
-  //프론트엔드에서 는 일이므로 굳이추가 안해도 될듯
-  // exports.onlyAuthed = (req, res, next) => {
-  //   if(req.session.isLoggedIn){
-  //       return next();
-  //   }
-  
-  // }
-  
-  // exports.onlyAnnoymous = (req, res, next) => {
-  //   if(!req.session.isLoggedIn){
-  //     return next();
-  // }
-  
-  // }
+
+//프론트엔드에서 는 일이므로 굳이추가 안해도 될듯
+// exports.onlyAuthed = (req, res, next) => {
+//   if(req.session.isLoggedIn){
+//       return next();
+//   }
+
+// }
+
+// exports.onlyAnnoymous = (req, res, next) => {
+//   if(!req.session.isLoggedIn){
+//     return next();
+// }
+
+// }
+
+const jwt = require("jsonwebtoken");
+const User = require("./schemas/user");
+
+exports.authMiddleWare = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (authorization !== null || authorization !== undefined) {
+    const token = authorization.split(" ").pop();
+    const { nickname } = token.verify(token, "secret");
+    User.findOne({ nickname }).then((result) => {
+      res.locals.user = result;
+    });
+  }
+
+  next();
+};
